69fe41d063d18fc7bdb9105da7f1e30a31a1671c
Stephen <stephenliew360@gmail.com>
2025-07-13 15:22:06 -0700

    stable version release

--- main and reasoning- base.py
+++ main and reasoning- base.py
@@ -1,19 +1,21 @@
 import yfinance as yf
 from openai import OpenAI
+import requests
+import json
 
-#data pipeline
+# data pipeline
 
-#tech news
+# tech news
 url = "https://www.alphavantage.co/query?function=NEWS_SENTIMENT&tickers=AAPL&apikey=DXL7K0GZEMR3P8RZ"
 r = requests.get(url)
 news = r.json()
 
-#insider trading
+# insider trading
 url = "https://www.alphavantage.co/query?function=INSIDER_TRANSACTIONS&symbol=IBM&apikey=DXL7K0GZEMR3P8RZ" 
 r = requests.get(url)
 insidertrading = r.json()
 
-#top gain and losers
+# top gain and losers
 url = "https://www.alphavantage.co/query?function=TOP_GAINERS_LOSERS&apikey=DXL7K0GZEMR3P8RZ"
 r = requests.get(url)
 gandl = r.json()
@@ -46,16 +48,17 @@ completion = client.chat.completions.create(
         {"role": "user", "content": prompt}
     ]
 )
+print(completion.choices[0].message.content)
 
-layerone = completion.choices[0].message.content.strip().split()
+# layerone = completion.choices[0].message.content.strip().split()
 
-clean_symbols = [s.strip().replace('$', '').replace(',', '').upper() for s in layerone[:5]]
+# clean_symbols = [s.strip().replace('$', '').replace(',', '').upper() for s in layerone[:5]]
 
-symbols = yf.Tickers(clean_symbols)
-count = 0
-for symbol in clean_symbols:
-    ticker = symbols.tickers[symbol]
-    recs = ticker.recommendations
-    hist = ticker.history(period="5d")
-    print(symbol, hist, "\n", recs, "\n")
+# symbols = yf.Tickers(clean_symbols)
+# count = 0
+# for symbol in clean_symbols:
+#     ticker = symbols.tickers[symbol]
+#     recs = ticker.recommendations
+#     hist = ticker.history(period="5d")
+#     print(symbol, hist, "\n", recs, "\n")
 
